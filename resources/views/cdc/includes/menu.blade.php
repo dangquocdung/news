f81f94bbc79b5c1c2bda9a5d9b11cf2878460a93
dangquocdung <dungthinhvn@gmail.com>
2019-04-08 15:41:34 +0700

    ádsa

--- resources/views/cdc/includes/menu.blade.php
+++ resources/views/cdc/includes/menu.blade.php
@@ -88,6 +88,22 @@
                                             </li>    
                                         @else
 
+                                            <?php
+                                                if ($HeaderMenuLink->webmasterSection[$slug_var] != "" && Helper::GeneralWebmasterSettings("links_status")) {
+                                                    if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                                                        $mmnnuu_link = url(trans('backLang.code')."/" .$HeaderMenuLink->webmasterSection[$slug_var]);
+                                                    }else{
+                                                        $mmnnuu_link = url($HeaderMenuLink->webmasterSection[$slug_var]);
+                                                    }
+                                                }else{
+                                                    if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                                                        $mmnnuu_link =url(trans('backLang.code')."/" .$HeaderMenuLink->webmasterSection['name']);
+                                                    }else{
+                                                        $mmnnuu_link =url($HeaderMenuLink->webmasterSection['name']);
+                                                    }
+                                                }
+                                            ?>
+
                                             <li>
                                                 <a href="{{ (trim($HeaderMenuLink->link) !="") ? $HeaderMenuLink->link:$mmnnuu_link }}">{{ $HeaderMenuLink->$title_vi }}</a>
                                             </li>
