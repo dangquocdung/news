74341366c527393aa0b754176078266271a024cc
dangquocdung <dungthinhvn@gmail.com>
2019-04-10 18:57:04 +0700

    asdsad

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -359,7 +359,7 @@ class TopicsController extends Controller
                 $e_details = $e_details."<br><br><a href='".$item->link."'><strong>Nguồn bài viết</strong></a>";
                 $e_date = $item->pubDate;
 
-                $exist = Topic::where('title_vi','LIKE',$e_title)->first();
+                $exist = Topic::where('title_vi','LIKE',$e_title)->get();
 
                 if ( count($exist) < 1 ){
 
