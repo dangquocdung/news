5e7211031a74b867e2941d6b307bc2dd84e7ad51
dangquocdung <dungthinhvn@gmail.com>
2019-04-22 17:39:43 +0700

    ádsa

--- resources/views/tinht/home/tin-anh.blade.php
+++ resources/views/tinht/home/tin-anh.blade.php
@@ -1,54 +1,58 @@
-<div class="row mb-10">  
-    <div class="Head pos-rel clearfix"> 
-        <h2 class="ParentCate left"> <a href="http://www.tinht.com/vi/hinh-anh">Tin ảnh</a> </h2> 
-        <span class="line-red">.</span>  
-    </div> 
-</div>
+<div class="col-md-12 mt-15 clearfix">
+    <div class="row">
+        <div class="Head pos-rel clearfix">
+            <h2 class="ParentCate left">
+                <a href="/hinh-anh">Hình ảnh</a>
+            </h2>
+            <span class="line-red">.</span>
+        </div>
+    </div>
 
-<div class="three-column-post-carousel column-post-carousel post-block-carousel dark life-style-post-carousel row">
+    <div class="three-column-post-carousel column-post-carousel post-block-carousel dark life-style-post-carousel row">
 
-    @foreach ($Photo as $Topic )
-        @php
-            if ($Topic->$slug_var != "" && Helper::GeneralWebmasterSettings("links_status")) {
-                if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                    $topic_link_url = url(trans('backLang.code') . "/" . $Topic->$slug_var);
+        @foreach ($Photo as $Topic )
+            @php
+                if ($Topic->$slug_var != "" && Helper::GeneralWebmasterSettings("links_status")) {
+                    if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                        $topic_link_url = url(trans('backLang.code') . "/" . $Topic->$slug_var);
+                    } else {
+                        $topic_link_url = url($Topic->$slug_var);
+                    }
                 } else {
-                    $topic_link_url = url($Topic->$slug_var);
+                    if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                        $topic_link_url = route('FrontendTopicByLang', ["lang" => trans('backLang.code'), "section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
+                    } else {
+                        $topic_link_url = route('FrontendTopic', ["section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
+                    }
                 }
-            } else {
-                if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                    $topic_link_url = route('FrontendTopicByLang', ["lang" => trans('backLang.code'), "section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                } else {
-                    $topic_link_url = route('FrontendTopic', ["section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                }
-            }
-        @endphp
-    
-        <!-- Overlay Post Start -->
-        <div class="post post-large post-overlay hero-post col-md-4">
-            <div class="post-wrap">
+            @endphp
+        
+            <!-- Overlay Post Start -->
+            <div class="post post-large post-overlay hero-post col-md-4">
+                <div class="post-wrap">
 
-                <!-- Image -->
-                <a class="image img-fluid" href="{{ $topic_link_url }}">
-                    @if (empty($Topic->photo_file))
-                        <img src="frontend/hashnews/img/post/post-42.jpg" alt="{{ $Topic->title_vi }}">
-                    @else
-                        <img src="/uploads/topics/{{ $Topic->photo_file}}" alt="{{ $Topic->title_vi }}"></a>
-                    @endif
-                </a>
+                    <!-- Image -->
+                    <a class="image img-fluid" href="{{ $topic_link_url }}">
+                        @if (empty($Topic->photo_file))
+                            <img src="frontend/hashnews/img/post/post-42.jpg" alt="{{ $Topic->title_vi }}">
+                        @else
+                            <img src="/uploads/topics/{{ $Topic->photo_file}}" alt="{{ $Topic->title_vi }}"></a>
+                        @endif
+                    </a>
 
-                <!-- Content -->
-                <div class="content" style="bottom:10px">
+                    <!-- Content -->
+                    <div class="content" style="bottom:10px">
 
-                    <!-- Title -->
-                    <h4 style="text-align:center; font-weght:bold; color:#fff"><a href="{{$topic_link_url}}" onMouseOver="this.style.color='#0F0'" onMouseOut="this.style.color='#fff'" >{{$Topic->title_vi}}</a></h4>
+                        <!-- Title -->
+                        <h4 style="text-align:center; font-weght:bold; color:#fff"><a href="{{$topic_link_url}}" onMouseOver="this.style.color='#0F0'" onMouseOut="this.style.color='#fff'" >{{$Topic->title_vi}}</a></h4>
 
+                    </div>
+                    
                 </div>
-                
-            </div>
-        </div><!-- Overlay Post End -->
-    @endforeach
-    
+            </div><!-- Overlay Post End -->
+        @endforeach
+        
+    </div>
 </div>
         
     
