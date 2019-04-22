40dfb4d8830df7912750b2eb144e6f9b763c698d
dangquocdung <dungthinhvn@gmail.com>
2019-04-22 17:01:17 +0700

    sdfds

--- resources/views/tinht/home/tin-anh.blade.php
+++ resources/views/tinht/home/tin-anh.blade.php
@@ -1,119 +1,26 @@
-<!-- Post Block Wrapper Start -->
-<div class="post-block-wrapper dark">
+<div class="three-column-post-carousel column-post-carousel post-block-carousel dark life-style-post-carousel row">
     
-    <!-- Post Block Head Start -->
-    <div class="head life-style-head">
-        
-        <!-- Title -->
-        <h4 class="title">Recent News</h4>
-        
-    </div><!-- Post Block Head End -->
-    
-    <!-- Post Block Body Start -->
-    <div class="body">
-        
-        <div class="three-column-post-carousel column-post-carousel post-block-carousel dark life-style-post-carousel row">
-            
-            <!-- Post Start -->
-            <div class="post post-dark life-style-post col">
-                <div class="post-wrap">
+    <!-- Post Start -->
+    <div class="post post-dark life-style-post col">
+        <div class="post-wrap">
 
-                    <!-- Image -->
-                    <a class="image" href="post-details.html"><img src="img/post/post-83.jpg" alt="post"></a>
+            <!-- Image -->
+            <a class="image" href="post-details.html"><img src="img/post/post-83.jpg" alt="post"></a>
 
-                    <!-- Content -->
-                    <div class="content">
+            <!-- Content -->
+            <div class="content">
 
-                        <!-- Title -->
-                        <h5 class="title"><a href="post-details.html">Liverpool’s attact make easy work of Arsenal madrid.</a></h5>
+                <!-- Title -->
+                <h5 class="title"><a href="post-details.html">Liverpool’s attact make easy work of Arsenal madrid.</a></h5>
 
-                        <!-- Meta -->
-                        <div class="meta fix">
-                            <a href="#" class="meta-item author"><i class="fa fa-user"></i> Sathi Bhuiyan</a>
-                            <span class="meta-item date"><i class="fa fa-clock-o"></i>10 March 2017</span>
-                        </div>
+            </div>
 
-                    </div>
-
-                </div>
-            </div><!-- Post End -->
-            
-            <!-- Post Start -->
-            <div class="post post-dark life-style-post col">
-                <div class="post-wrap">
-
-                    <!-- Image -->
-                    <a class="image" href="post-details.html"><img src="img/post/post-84.jpg" alt="post"></a>
-
-                    <!-- Content -->
-                    <div class="content">
-
-                        <!-- Title -->
-                        <h5 class="title"><a href="post-details.html">Marcel kittle wins stage to of the tour de France.</a></h5>
-
-                        <!-- Meta -->
-                        <div class="meta fix">
-                            <a href="#" class="meta-item author"><i class="fa fa-user"></i> Sathi Bhuiyan</a>
-                            <span class="meta-item date"><i class="fa fa-clock-o"></i>10 March 2017</span>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div><!-- Post End -->
-            
-            <!-- Post Start -->
-            <div class="post post-dark life-style-post col">
-                <div class="post-wrap">
-
-                    <!-- Image -->
-                    <a class="image" href="post-details.html"><img src="img/post/post-85.jpg" alt="post"></a>
-
-                    <!-- Content -->
-                    <div class="content">
-
-                        <!-- Title -->
-                        <h5 class="title"><a href="post-details.html">NHI Fantasy , Fantasy hockey lockey primer for the fastern.</a></h5>
-
-                        <!-- Meta -->
-                        <div class="meta fix">
-                            <a href="#" class="meta-item author"><i class="fa fa-user"></i> Sathi Bhuiyan</a>
-                            <span class="meta-item date"><i class="fa fa-clock-o"></i>10 March 2017</span>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div><!-- Post End -->
-            
-            <!-- Post Start -->
-            <div class="post post-dark life-style-post col">
-                <div class="post-wrap">
-
-                    <!-- Image -->
-                    <a class="image" href="post-details.html"><img src="img/post/post-86.jpg" alt="post"></a>
-
-                    <!-- Content -->
-                    <div class="content">
-
-                        <!-- Title -->
-                        <h5 class="title"><a href="post-details.html">Liverpool’s attact make easy work of Arsenal madrid.</a></h5>
-
-                        <!-- Meta -->
-                        <div class="meta fix">
-                            <a href="#" class="meta-item author"><i class="fa fa-user"></i> Sathi Bhuiyan</a>
-                            <span class="meta-item date"><i class="fa fa-clock-o"></i>10 March 2017</span>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div><!-- Post End -->
-            
         </div>
-        
-    </div><!-- Post Block Body End -->
+    </div><!-- Post End -->
     
-</div><!-- Post Block Wrapper End -->
-                
+   
+    
+</div>
+        
+    
             
