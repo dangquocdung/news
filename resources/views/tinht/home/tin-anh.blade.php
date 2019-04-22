e01b25485a14815501e1e8acda414c4d7f2e055d
dangquocdung <dungthinhvn@gmail.com>
2019-04-22 16:47:39 +0700

    sdfd

--- resources/views/tinht/home/tin-anh.blade.php
+++ resources/views/tinht/home/tin-anh.blade.php
@@ -1,81 +1,133 @@
-@if (!empty($Photo))
-    
-    <!-- Post Block Wrapper Start -->
-    <div class="post-block-wrapper mb-15">
+ <!-- Recent Section Start -->
+ <div class="recent-section section bg-dark pt-50 pb-50">
+    <div class="container">
         
-        <!-- Post Block Head Start -->
-        <div class="head">
+        <!-- Feature Post Row Start -->
+        <div class="row">
             
-            <!-- Title -->
-            <h4 class="title">Tin Ảnh</h4>
-            
-        </div><!-- Post Block Head End -->
-        
-        <!-- Post Block Body Start -->
-        <div class="body">
-            
-            <div class="two-column-post-carousel column-post-carousel post-block-carousel row">
-
-                @foreach($Photo as $Topic)
-
-                    <?php
-                        if ($Topic->$slug_var != "" && Helper::GeneralWebmasterSettings("links_status")) {
-                            if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                                $topic_link_url = url(trans('backLang.code') . "/" . $Topic->$slug_var);
-                            } else {
-                                $topic_link_url = url($Topic->$slug_var);
-                            }
-                        } else {
-                            if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                                $topic_link_url = route('FrontendTopicByLang', ["lang" => trans('backLang.code'), "section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                            } else {
-                                $topic_link_url = route('FrontendTopic', ["section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                            }
-                        }
-                    ?>
+            <div class="col-12">
                 
-                    <div class="col-md-6 col-12">
-                        
-                        <!-- Overlay Post Start -->
-                        <div class="post hero-post">
-                            <div class="post-wrap">
-
-                                <!-- Image -->
-
-                                <div class="image">
-                                    <a href="{{ $topic_link_url }}">
-                                        @if ($Topic->photo_file != null && file_exists('uploads/topics/'.$Topic->photo_file))
-                                            <img src="uploads/topics/{{ $Topic->photo_file }}" alt="{{ $Topic->title_vi}}" style="max-height:240px;">
-                                        @else
-                                            <img src="tinht/img/post/post-48.jpg" alt="{{ $Topic->title_vi}}">
-                                        @endif
-                                    </a>
-                                </div>
-
-                                <!-- Content -->
-                                <div class="content-tlq">
-
-                                    <!-- Title -->
-                                    <h5 class="title" style="margin-top:10px; text-align:center"><a href="{{ $topic_link_url }}"> {{ $Topic->title_vi }}</a></h5>
-
-                                    <!-- Meta -->
-                                    {{--  <div class="meta fix">
-                                        <span class="meta-item date"><i class="fa fa-clock-o"></i>{{ \Carbon\Carbon::parse($Topic->date)->format('d/m/Y') }}</span>
-                                    </div>  --}}
-
-                                </div>
-
-                            </div>
-                        </div><!-- Overlay Post End -->
+                <!-- Post Block Wrapper Start -->
+                <div class="post-block-wrapper dark">
                     
-                    </div>
+                    <!-- Post Block Head Start -->
+                    <div class="head life-style-head">
+                        
+                        <!-- Title -->
+                        <h4 class="title">Recent News</h4>
+                        
+                    </div><!-- Post Block Head End -->
+                    
+                    <!-- Post Block Body Start -->
+                    <div class="body">
+                        
+                        <div class="three-column-post-carousel column-post-carousel post-block-carousel dark life-style-post-carousel row">
+                            
+                            <!-- Post Start -->
+                            <div class="post post-dark life-style-post col">
+                                <div class="post-wrap">
 
-                @endforeach
+                                    <!-- Image -->
+                                    <a class="image" href="post-details.html"><img src="img/post/post-83.jpg" alt="post"></a>
 
+                                    <!-- Content -->
+                                    <div class="content">
+
+                                        <!-- Title -->
+                                        <h5 class="title"><a href="post-details.html">Liverpool’s attact make easy work of Arsenal madrid.</a></h5>
+
+                                        <!-- Meta -->
+                                        <div class="meta fix">
+                                            <a href="#" class="meta-item author"><i class="fa fa-user"></i> Sathi Bhuiyan</a>
+                                            <span class="meta-item date"><i class="fa fa-clock-o"></i>10 March 2017</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div><!-- Post End -->
+                            
+                            <!-- Post Start -->
+                            <div class="post post-dark life-style-post col">
+                                <div class="post-wrap">
+
+                                    <!-- Image -->
+                                    <a class="image" href="post-details.html"><img src="img/post/post-84.jpg" alt="post"></a>
+
+                                    <!-- Content -->
+                                    <div class="content">
+
+                                        <!-- Title -->
+                                        <h5 class="title"><a href="post-details.html">Marcel kittle wins stage to of the tour de France.</a></h5>
+
+                                        <!-- Meta -->
+                                        <div class="meta fix">
+                                            <a href="#" class="meta-item author"><i class="fa fa-user"></i> Sathi Bhuiyan</a>
+                                            <span class="meta-item date"><i class="fa fa-clock-o"></i>10 March 2017</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div><!-- Post End -->
+                            
+                            <!-- Post Start -->
+                            <div class="post post-dark life-style-post col">
+                                <div class="post-wrap">
+
+                                    <!-- Image -->
+                                    <a class="image" href="post-details.html"><img src="img/post/post-85.jpg" alt="post"></a>
+
+                                    <!-- Content -->
+                                    <div class="content">
+
+                                        <!-- Title -->
+                                        <h5 class="title"><a href="post-details.html">NHI Fantasy , Fantasy hockey lockey primer for the fastern.</a></h5>
+
+                                        <!-- Meta -->
+                                        <div class="meta fix">
+                                            <a href="#" class="meta-item author"><i class="fa fa-user"></i> Sathi Bhuiyan</a>
+                                            <span class="meta-item date"><i class="fa fa-clock-o"></i>10 March 2017</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div><!-- Post End -->
+                            
+                            <!-- Post Start -->
+                            <div class="post post-dark life-style-post col">
+                                <div class="post-wrap">
+
+                                    <!-- Image -->
+                                    <a class="image" href="post-details.html"><img src="img/post/post-86.jpg" alt="post"></a>
+
+                                    <!-- Content -->
+                                    <div class="content">
+
+                                        <!-- Title -->
+                                        <h5 class="title"><a href="post-details.html">Liverpool’s attact make easy work of Arsenal madrid.</a></h5>
+
+                                        <!-- Meta -->
+                                        <div class="meta fix">
+                                            <a href="#" class="meta-item author"><i class="fa fa-user"></i> Sathi Bhuiyan</a>
+                                            <span class="meta-item date"><i class="fa fa-clock-o"></i>10 March 2017</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div><!-- Post End -->
+                            
+                        </div>
+                        
+                    </div><!-- Post Block Body End -->
+                    
+                </div><!-- Post Block Wrapper End -->
+                
             </div>
             
-        </div><!-- Post Block Body End -->
+        </div><!-- Feature Post Row End -->
         
-    </div><!-- Post Block Wrapper End -->
-
-@endif
+    </div>
+</div><!-- Popular Section End -->
