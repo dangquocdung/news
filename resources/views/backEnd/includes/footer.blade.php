f3ccd9938bfc0cf57ab80adad1b83f703a2fd811
dangquocdung <dungthinhvn@gmail.com>
2019-04-25 11:21:19 +0700

    áda

--- resources/views/backEnd/includes/footer.blade.php
+++ resources/views/backEnd/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="app-footer">
     <div class="p-a text-xs">
         <div class="pull-right text-muted">
-            &copy; Bản quyền thuộc về <strong>CDC Hà Tĩnh </strong> <span
+            &copy; Bản quyền thuộc về <strong>Phần mềm Dũng Thịnh</strong> <span
                     class="hidden-xs-down">- <?php echo date("Y") ?></span>
             <a ui-scroll-to="content"><i class="fa fa-long-arrow-up p-x-sm"></i></a>
         </div>
