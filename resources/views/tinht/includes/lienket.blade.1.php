c26bc04c0ef7cf738c811160e6160c2663144eb3
dangquocdung <dungthinhvn@gmail.com>
2019-04-26 13:53:12 +0700

    ádsa

--- resources/views/tinht/includes/lienket.blade.1.php
+++ resources/views/tinht/includes/lienket.blade.1.php
@@ -97,3 +97,67 @@
     </div>
             
 </div>
+
+<div class="col-md-12 mt-15 clearfix">
+    <div class="row">
+        <div class="Head pos-rel clearfix">
+            <h2 class="ParentCate left">
+                <a href="#">Âm nhạc</a>
+            </h2>
+            <span class="line-red">.</span>
+        </div>
+    </div>
+            
+    <div class="row sidebar">
+
+        <div id="phat-thanh" style="overflow:hidden">
+
+            <audio id="audio" preload="auto" tabindex="0" controls="" type="audio/mpeg">
+                <source type="audio/mp3" src="">
+                Sorry, your browser does not support HTML5 audio.
+            </audio>
+            <ul class="title" id="playlist">
+                @foreach ($Audio as $Topic )
+                    <li class="mb-1">
+                        <a href="/uploads/topics/{{ $Topic->audio_file}}">
+                            <i class="fa fa-volume-up" aria-hidden="true" style="font-size:20px;color:blue;"></i>  {{$Topic->title_vi}}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        
+    </div>
+            
+</div>
+
+<div class="col-md-12 mt-15 clearfix">
+    <div class="row">
+        <div class="Head pos-rel clearfix">
+            <h2 class="ParentCate left">
+                <a href="#">Liên kết</a>
+            </h2>
+            <span class="line-red">.</span>
+        </div>
+    </div>
+            
+    <div class="row post-wrap">
+
+        <ul>
+
+            @foreach($Banners->where('section_id',6) as $Banner)
+
+                <li>
+
+                    <a href="{{ $Banner->link_url }}" style="width:100%" class="mb-1">
+                        <img class="img-fluid" src="/uploads/banners/{{ $Banner->file_vi}}" width="100%"/>
+                    </a>
+
+                </li>
+
+            @endforeach
+        </ul>
+
+    </div>
+            
+</div>
