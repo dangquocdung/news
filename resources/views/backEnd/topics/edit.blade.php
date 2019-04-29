2cc144da3f250f9ada453257d3eac7fd5b5a0ecd
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-04-29 18:02:41 +0700

    sdfs

--- resources/views/backEnd/topics/edit.blade.php
+++ resources/views/backEnd/topics/edit.blade.php
@@ -1128,8 +1128,8 @@
 
                                                 <div class="clearfix"></div>
                                                    
-                                                {!! Form::text('description_'.$photo->id,$photo->description, array('class' => 'pull-left form-control','id'=>'description','style'=>'margin:0; font-size:12px;')) !!}
-                                                
+                                                {!! Form::textarea('description_'.$photo->id,$photo->description, array('class' => 'pull-left form-control mb-1',rows' => '3','id'=>'description','style'=>'font-size:12px;')) !!}
+
 
                                                 <img class="img-responsive" src="{{ URL::to('uploads/topics/'.$photo->file) }}"
                                                 alt="{{ $photo->title  }}" title="{{ $photo->title  }}">
