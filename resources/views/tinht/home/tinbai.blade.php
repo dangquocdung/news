578cbeef13a7d35a78169b44bb80db766beb62b6
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-01 18:38:41 +0700

    sadad

--- resources/views/tinht/home/tinbai.blade.php
+++ resources/views/tinht/home/tinbai.blade.php
@@ -75,123 +75,129 @@
                             
                         @endphp
 
-                        
-                    
+                        <div class="col-md-12">
 
-                        <!-- Post Wrapper Start -->
+                            <!-- Post Wrapper Start -->
                         <div class="col-md-7 col-xs-12">
-                            @php
-                            
-                                $Topic =  $Tins->shift();
-    
-                                if ($Topic->$slug_var != "" && Helper::GeneralWebmasterSettings("links_status")) {
-                                    if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                                        $topic_link_url = url(trans('backLang.code') . "/" . $Topic->$slug_var);
-                                    } else {
-                                        $topic_link_url = url($Topic->$slug_var);
-                                    }
-                                } else {
-                                    if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                                        $topic_link_url = route('FrontendTopicByLang', ["lang" => trans('backLang.code'), "section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                                    } else {
-                                        $topic_link_url = route('FrontendTopic', ["section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                                    }
-                                }
+                                @php
                                 
-                            @endphp
-
-
-                                    <!-- Post Start -->
-                                <div class="post feature-post post-separator-border">
-                                    <div class="post-wrap">
-
-                                        <!-- Image -->
-                                        <a class="image img-fluid" href="{{ $topic_link_url }}">
-                                            @if (empty($Topic->photo_file))
-                                                <img src="frontend/hashnews/img/post/post-11.jpg" alt="{{ $Topic->title_vi }}">
-                                            @else
-                                                <img src="/uploads/topics/{{ $Topic->photo_file}}" alt="{{ $Topic->title_vi }}"></a>
-                                            @endif
-                                        </a>
-
-                                        <!-- Content -->
-                                        <div class="content">
-
-                                            <!-- Title -->
-                                            <h4 class="title" style="text-align:center">
-                                                <a href="{{ $topic_link_url }}">
-                                                        @if($Topic->icon !="")
-                                                        <i class="fa {!! $Topic->icon !!}"></i>&nbsp;
-                                                    @endif
-                                                    {{ $Topic->title_vi }}
-                                                </a>
-                                            </h4>
-                                            <!-- Description -->
-                                            <p class="sapo">{{ $Topic->sapo }}</p>
-
-                                        </div>
-                                        
-                                    </div>
-                                </div><!-- Post End -->
-
-                        </div><!-- Post Wrapper End -->
-
-                            <!-- Small Post Wrapper Start -->
-                            <div class="col-md-5 col-xs-12" style="padding-right:0" id="mainbox-r">
-                            
-                                @foreach ($Tins as $Topic )
-
-                                    @php
-                                        if ($Topic->$slug_var != "" && Helper::GeneralWebmasterSettings("links_status")) {
-                                            if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                                                $topic_link_url = url(trans('backLang.code') . "/" . $Topic->$slug_var);
-                                            } else {
-                                                $topic_link_url = url($Topic->$slug_var);
-                                            }
+                                    $Topic =  $Tins->shift();
+        
+                                    if ($Topic->$slug_var != "" && Helper::GeneralWebmasterSettings("links_status")) {
+                                        if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                                            $topic_link_url = url(trans('backLang.code') . "/" . $Topic->$slug_var);
                                         } else {
-                                            if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                                                $topic_link_url = route('FrontendTopicByLang', ["lang" => trans('backLang.code'), "section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                                            } else {
-                                                $topic_link_url = route('FrontendTopic', ["section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                                            }
+                                            $topic_link_url = url($Topic->$slug_var);
                                         }
-                                    @endphp
-
-                                    <!-- Post Small Start -->
-                                    <div class="post post-small post-list feature-post post-separator-border">
+                                    } else {
+                                        if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                                            $topic_link_url = route('FrontendTopicByLang', ["lang" => trans('backLang.code'), "section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
+                                        } else {
+                                            $topic_link_url = route('FrontendTopic', ["section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
+                                        }
+                                    }
+                                    
+                                @endphp
+    
+    
+                                        <!-- Post Start -->
+                                    <div class="post feature-post post-separator-border">
                                         <div class="post-wrap">
-
+    
                                             <!-- Image -->
-                                            {{-- <a class="image img-fluid" href="{{ $topic_link_url }}">
+                                            <a class="image img-fluid" href="{{ $topic_link_url }}">
                                                 @if (empty($Topic->photo_file))
-                                                    <img src="frontend/hashnews/img/post/post-13.jpg" alt="{{ $Topic->title_vi }}">
+                                                    <img src="frontend/hashnews/img/post/post-11.jpg" alt="{{ $Topic->title_vi }}">
                                                 @else
-                                                    <img src="/uploads/topics/{{ $Topic->photo_file}}" alt="{{ $Topic->title_vi }}">
+                                                    <img src="/uploads/topics/{{ $Topic->photo_file}}" alt="{{ $Topic->title_vi }}"></a>
                                                 @endif
-                                            </a> --}}
-
+                                            </a>
+    
                                             <!-- Content -->
                                             <div class="content">
-
+    
                                                 <!-- Title -->
-                                                <h5 class="title">
+                                                <h4 class="title" style="text-align:center">
                                                     <a href="{{ $topic_link_url }}">
                                                             @if($Topic->icon !="")
                                                             <i class="fa {!! $Topic->icon !!}"></i>&nbsp;
                                                         @endif
                                                         {{ $Topic->title_vi }}
                                                     </a>
-                                                </h5>
-
-                                                
-
+                                                </h4>
+                                                <!-- Description -->
+                                                <p class="sapo">{{ $Topic->sapo }}</p>
+    
                                             </div>
                                             
                                         </div>
-                                    </div><!-- Post Small End -->
-                                @endforeach
-                            
-                        </div><!-- Small Post Wrapper End -->
+                                    </div><!-- Post End -->
+    
+                            </div><!-- Post Wrapper End -->
+    
+                                <!-- Small Post Wrapper Start -->
+                            <div class="col-md-5 col-xs-12" style="padding-right:0" id="mainbox-r">
+                                
+                                    @foreach ($Tins as $Topic )
+    
+                                        @php
+                                            if ($Topic->$slug_var != "" && Helper::GeneralWebmasterSettings("links_status")) {
+                                                if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                                                    $topic_link_url = url(trans('backLang.code') . "/" . $Topic->$slug_var);
+                                                } else {
+                                                    $topic_link_url = url($Topic->$slug_var);
+                                                }
+                                            } else {
+                                                if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
+                                                    $topic_link_url = route('FrontendTopicByLang', ["lang" => trans('backLang.code'), "section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
+                                                } else {
+                                                    $topic_link_url = route('FrontendTopic', ["section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
+                                                }
+                                            }
+                                        @endphp
+    
+                                        <!-- Post Small Start -->
+                                        <div class="post post-small post-list feature-post post-separator-border">
+                                            <div class="post-wrap">
+    
+                                                <!-- Image -->
+                                                {{-- <a class="image img-fluid" href="{{ $topic_link_url }}">
+                                                    @if (empty($Topic->photo_file))
+                                                        <img src="frontend/hashnews/img/post/post-13.jpg" alt="{{ $Topic->title_vi }}">
+                                                    @else
+                                                        <img src="/uploads/topics/{{ $Topic->photo_file}}" alt="{{ $Topic->title_vi }}">
+                                                    @endif
+                                                </a> --}}
+    
+                                                <!-- Content -->
+                                                <div class="content">
+    
+                                                    <!-- Title -->
+                                                    <h5 class="title">
+                                                        <a href="{{ $topic_link_url }}">
+                                                                @if($Topic->icon !="")
+                                                                <i class="fa {!! $Topic->icon !!}"></i>&nbsp;
+                                                            @endif
+                                                            {{ $Topic->title_vi }}
+                                                        </a>
+                                                    </h5>
+    
+                                                    
+    
+                                                </div>
+                                                
+                                            </div>
+                                        </div><!-- Post Small End -->
+                                    @endforeach
+                                
+                            </div><!-- Small Post Wrapper End -->
+
+                        </div>
+
+                        
+                    
+
+                        
 
                             
 
