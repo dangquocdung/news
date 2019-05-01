413905c97353534d69fb70daf02ac0752f411064
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-02 06:49:43 +0700

    asdas

--- resources/views/tinht/includes/head.blade.php
+++ resources/views/tinht/includes/head.blade.php
@@ -48,7 +48,6 @@
 
 <!-- jQuery JS -->
 {{-- <script src="{{ URL::asset('tinht/js/vendor/jquery-3.3.1.js') }}"></script> --}}
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
 
