1b5a8aa37f3c36570d3b2d169b3e11ec387e98c0
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-01 18:48:24 +0700

    sdfds

--- resources/views/tinht/home/video.blade.php
+++ resources/views/tinht/home/video.blade.php
@@ -1,23 +1,19 @@
 <div class="col-md-12 mt-15">
-    <div class="row">
-        <div class="Head pos-rel clearfix">
-            <h2 class="ParentCate left">
-                <a href="#">Video clips</a>
-            </h2>
-            <span class="line-red">.</span>
-        </div>
+    <div class="Head pos-rel clearfix">
+        <h2 class="ParentCate left">
+            <a href="#">Video clips</a>
+        </h2>
+        <span class="line-red">.</span>
     </div>
 
-    <div class="row">
-        <div class="RYPP r16-9" data-playlist="PLh_HV-iv7X8PHVkqrcy9qegZKJu-nMbbL">
-            <div class="RYPP-video">
-                <div class="RYPP-video-player">
-                <!-- Will be replaced -->
-                </div>
+    <div class="RYPP r16-9" data-playlist="PLh_HV-iv7X8PHVkqrcy9qegZKJu-nMbbL">
+        <div class="RYPP-video">
+            <div class="RYPP-video-player">
+            <!-- Will be replaced -->
             </div>
-            <div class="RYPP-playlist">
-                <div class="RYPP-items customScroll"></div>
-            </div>
+        </div>
+        <div class="RYPP-playlist">
+            <div class="RYPP-items customScroll"></div>
         </div>
     </div>
             
