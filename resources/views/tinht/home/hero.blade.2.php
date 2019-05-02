b6d829bfd5653516f1a28b505b220f34c6cbbf17
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-02 07:32:47 +0700

    sdsad

--- resources/views/tinht/home/hero.blade.2.php
+++ resources/views/tinht/home/hero.blade.2.php
@@ -6,69 +6,26 @@
 
             <div class="row">
 
-                <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        @foreach ($HotTopics as $key=>$Topic )
-                            <li data-target="#myCarousel" data-slide-to="0" class="{{ ($key==0)?'active':'' }}"></li>
-                        @endforeach
-                    </ol>
-                
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                        @foreach ($HotTopics as $key=>$Topic )
+                <!-- Hero Post Slider Start -->
+        <div class="post-carousel-1">
 
-                            @php
+        @foreach($Banners->where('section_id',9) as $key=>$SlideBanner)
 
-                                if ($Topic->$slug_var != "" && Helper::GeneralWebmasterSettings("links_status")) {
-                                    if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                                        $topic_link_url = url(trans('backLang.code') . "/" . $Topic->$slug_var);
-                                    } else {
-                                        $topic_link_url = url($Topic->$slug_var);
-                                    }
-                                } else {
-                                    if (trans('backLang.code') != env('DEFAULT_LANGUAGE')) {
-                                        $topic_link_url = route('FrontendTopicByLang', ["lang" => trans('backLang.code'), "section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                                    } else {
-                                        $topic_link_url = route('FrontendTopic', ["section" => $Topic->webmasterSection->name, "id" => $Topic->id]);
-                                    }
-                                }
-                                
-                            @endphp
+            <!-- Overlay Post Start -->
+            <div class="post post-large hero-post">
+                <div class="post-wrap">
 
-                            <div class="item {{ ($key==0)?'active':'' }}">
-                                @if (empty($Topic->photo_file))
-                                    <img src="frontend/hashnews/img/post/post-11.jpg" alt="{{ $Topic->title_vi }}">
-                                @else
-                                    <img src="/uploads/topics/{{ $Topic->photo_file}}" alt="{{ $Topic->title_vi }}"></a>
-                                @endif
-                                <div class="carousel-caption">
-                                <h4>
-                                    <a href="{{ $topic_link_url }}">
-                                        @if($Topic->icon !="")
-                                            <i class="fa {!! $Topic->icon !!}"></i>&nbsp;
-                                        @endif
-
-                                        {{ $Topic->title_vi }}
-                                    </a>
-                                </h4>
-                                <p class="sapo">{{ $Topic->sapo }}</p>
-                                </div>
-                            </div>
-                        @endforeach
-                
-                    </div>
-                
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                    <span class="sr-only">Previous</span>
+                    <!-- Image -->
+                    <a href="{{ $SlideBanner->link_url }}" class="image" style="margin-bottom: 0 !important;">
+                        <img src="/uploads/banners/{{ $SlideBanner->$file_var }}" alt="{{ $SlideBanner->$title_var }}">
                     </a>
-                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                    <span class="sr-only">Next</span>
-                    </a>
+                    
                 </div>
+            </div><!-- Overlay Post End -->
+
+        @endforeach
+
+        </div><!-- Hero Post Slider End -->
                 
             </div>
 
