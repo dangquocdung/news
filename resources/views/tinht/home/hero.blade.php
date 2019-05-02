04ac962a29eed9f23eaf84ea56c804a84c640329
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-02 09:48:59 +0700

    gjjh

--- resources/views/tinht/home/hero.blade.php
+++ resources/views/tinht/home/hero.blade.php
@@ -44,7 +44,7 @@
                                             </div>
 
                                             <!-- Category -->
-                                        <a href="#" class="category {{ $Topic->webmasterSection->name}}">{{ $Topic->webmasterSection->name}}</a>
+                                        <a href="#" class="category {{ $Topic->webmasterSection->name}}">{{ trans('backlang.'.$Topic->webmasterSection->name) }}</a>
 
                                             <!-- Content -->
                                             <div class="content">
