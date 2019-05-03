0568c64ff6f86e215ceafdc83291fbcf86ba62eb
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-03 22:37:36 +0700

    qwqwe

--- resources/views/tinht/includes/panolens.blade.php
+++ resources/views/tinht/includes/panolens.blade.php
@@ -43,11 +43,11 @@
                 autoHideInfospot: true,            // Auto hide infospots
                 horizontalView: false,            // Allow only horizontal camera control
                 cameraFov: 60,                // Camera field of view in degree
-                reverseDragging: false,            // Reverse orbit control direction
-                enableReticle: false,            // Enable reticle for mouseless interaction
+                reverseDragging: true,            // Reverse orbit control direction
+                enableReticle: true,            // Enable reticle for mouseless interaction
                 dwellTime: 1500,            // Dwell time for reticle selection in millisecond
                 autoReticleSelect: true,        // Auto select a clickable target after dwellTime
-                viewIndicator: false,            // Adds an angle view indicator in upper left corner
+                viewIndicator: true,            // Adds an angle view indicator in upper left corner
                 indicatorSize: 30,            // Size of View Indicator
                 output: 'console'            // Whether and where to output infospot position. Could be 'console' or 'overlay'
             });
