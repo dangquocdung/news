fa66ff48d62b6b29951325a6b33767448056585c
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-03 22:40:36 +0700

    asd

--- resources/views/tinht/includes/panolens.blade.php
+++ resources/views/tinht/includes/panolens.blade.php
@@ -38,18 +38,18 @@
             viewer = new PANOLENS.Viewer({
                 container: document.body,        // A DOM Element container
                 controlBar: true,             // Vsibility of bottom control bar
-                controlButtons: [],            // Buttons array in the control bar. Default to ['fullscreen', 'setting', 'video']
-                autoHideControlBar: true,        // Auto hide control bar
-                autoHideInfospot: true,            // Auto hide infospots
-                horizontalView: false,            // Allow only horizontal camera control
-                cameraFov: 60,                // Camera field of view in degree
-                reverseDragging: true,            // Reverse orbit control direction
-                enableReticle: true,            // Enable reticle for mouseless interaction
-                dwellTime: 1500,            // Dwell time for reticle selection in millisecond
-                autoReticleSelect: true,        // Auto select a clickable target after dwellTime
+                // controlButtons: [],            // Buttons array in the control bar. Default to ['fullscreen', 'setting', 'video']
+                // autoHideControlBar: true,        // Auto hide control bar
+                // autoHideInfospot: true,            // Auto hide infospots
+                // horizontalView: false,            // Allow only horizontal camera control
+                // cameraFov: 60,                // Camera field of view in degree
+                // reverseDragging: true,            // Reverse orbit control direction
+                // enableReticle: true,            // Enable reticle for mouseless interaction
+                // dwellTime: 1500,            // Dwell time for reticle selection in millisecond
+                // autoReticleSelect: true,        // Auto select a clickable target after dwellTime
                 viewIndicator: true,            // Adds an angle view indicator in upper left corner
-                indicatorSize: 30,            // Size of View Indicator
-                output: 'console'            // Whether and where to output infospot position. Could be 'console' or 'overlay'
+                // indicatorSize: 30,            // Size of View Indicator
+                // output: 'console'            // Whether and where to output infospot position. Could be 'console' or 'overlay'
             });
             viewer.add( panorama );
             
