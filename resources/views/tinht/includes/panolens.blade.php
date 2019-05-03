e8b1cb3e957acb532eb827549d3cf49c2434d72e
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-03 11:48:14 +0700

    sadas

--- resources/views/tinht/includes/panolens.blade.php
+++ resources/views/tinht/includes/panolens.blade.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width, shrink-to-fit=no">
+    <title>Panolens.js panorama image panorama</title>
+    <style>
+        html, body {
+          margin: 0;
+          width: 100%;
+          height: 100%;
+          overflow: hidden;
+          background-color: #000;
+        }
+  
+        a:link, a:visited{
+          color: #bdc3c7;
+        }
+  
+        .credit{
+          position: absolute;
+          text-align: center;
+          width: 100%;
+          padding: 20px 0;
+          color: #fff;
+        }
+      </style>
 </head>
+
+
 <body>
 
         <script src="{{ URL::asset('/tinht/js/three.min.js') }}"></script>
@@ -14,14 +36,17 @@
 
             var panorama, viewer;
 
-            assetPath = 'http://pchen66.github.io/Panolens/examples/asset/textures/equirectangular';
+
+
+            assetPath = '';
             
-            panorama = new PANOLENS.ImagePanorama( assetPath + '/view.jpg' );
+            panorama = new PANOLENS.ImagePanorama('http://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/view.jpg');
             
             viewer = new PANOLENS.Viewer();
             viewer.add( panorama );
             
-        </script>
+        </script>            
+
     
 </body>
 </html>
