a762b05c3ca84b4380d23cd3f66b08cae7ed97c3
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-03 22:27:20 +0700

    asd

--- resources/views/tinht/includes/panolens.blade.php
+++ resources/views/tinht/includes/panolens.blade.php
@@ -35,7 +35,22 @@
             var photo_file = topic.attach_file;
             
             panorama = new PANOLENS.ImagePanorama(assetPath + photo_file);
-            viewer = new PANOLENS.Viewer();
+            viewer = new PANOLENS.Viewer({
+                container: document.body,        // A DOM Element container
+                controlBar: true,             // Vsibility of bottom control bar
+                controlButtons: [],            // Buttons array in the control bar. Default to ['fullscreen', 'setting', 'video']
+                autoHideControlBar: false,        // Auto hide control bar
+                autoHideInfospot: true,            // Auto hide infospots
+                horizontalView: false,            // Allow only horizontal camera control
+                cameraFov: 60,                // Camera field of view in degree
+                reverseDragging: false,            // Reverse orbit control direction
+                enableReticle: false,            // Enable reticle for mouseless interaction
+                dwellTime: 1500,            // Dwell time for reticle selection in millisecond
+                autoReticleSelect: true,        // Auto select a clickable target after dwellTime
+                viewIndicator: false,            // Adds an angle view indicator in upper left corner
+                indicatorSize: 30,            // Size of View Indicator
+                output: 'console'            // Whether and where to output infospot position. Could be 'console' or 'overlay'
+            });
             viewer.add( panorama );
             
         </script>            
