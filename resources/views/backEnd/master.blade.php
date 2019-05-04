82411b8b7be84409dfb88a6598412c2abe9b407d
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-05 00:04:59 +0700

    asd

--- resources/views/backEnd/master.blade.php
+++ resources/views/backEnd/master.blade.php
@@ -1,36 +1,133 @@
-@extends('master')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-    <div class="main-content-wrapper">
-        <a href="javascript:;" class="btn btn-info" data-type="rv-media" data-target="#description" data-editor="summernote">
-            <i class="fa fa-picture-o" aria-hidden="true"></i> {{ __('Add Media') }}
-        </a>
-        <textarea name="description" id="description" class="summernote form-control"></textarea>
+    <title>RvMedia - Laravel Media Management</title>
+    <meta name="description" content="RvMedia is used to manage media in Laravel project, it can works with Laravel 5.x and easy to setup. You will have many features with this media management.">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <meta property="og:image" content="{{ url('images/banner.png') }}">
+    <meta property="og:description" content="RvMedia is used to manage media in Laravel project, it can works with Laravel 5.x and easy to setup. You will have many features with this media management.">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #eee !important;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        body .navbar-default {
+            background-color: #3097d1;
+            border-color: #2984b7;
+            border-radius: 0;
+            position: relative;
+            min-height: 50px;
+            color: #fff;
+            z-index: 9999;
+            margin-bottom : 0;
+        }
+        body .navbar-default .navbar-brand,
+        body .navbar-default .nav > li > a {
+            color: #fff !important;
+        }
+
+        body .navbar-default .nav > li > a:hover {
+            color: #c9e2f9 !important;
+        }
+        .main-content-wrapper {
+            max-width: 700px;
+            margin: 50px auto;
+        }
+        .main-content-wrapper .btn.btn-info {
+            margin-bottom : 10px;
+        }
+        #docs-navbar .btn-group {
+            margin-top : 9px;
+        }
+        #docs-navbar .btn-group .dropdown-toggle {
+            border: none !important;
+            background: none !important;
+            box-shadow: none !important;
+            color: #fff !important;
+            font-family: 'Raleway', sans-serif;
+        }
+        #docs-navbar .dropdown-menu {
+            -webkit-border-radius : 0;
+            -moz-border-radius    : 0;
+            border-radius         : 0;
+        }
+        @media (min-width: 768px) {
+            .modal-dialog {
+                margin : 90px auto !important;
+            }
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#docs-navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="{{ url('/') }}" class="navbar-brand">RvMedia</a>
+        </div>
+        <div id="docs-navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="{{ url('/') }}">Media</a></li>
+                <li><a href="{{ route('integration.ckeditor') }}">CkEditor</a></li>
+                <li><a href="{{ route('integration.summernote') }}">Summernote</a></li>
+                <li><a href="{{ route('integration.wysihtml5') }}">Wysihtml5</a></li>
+                <li><a href="{{ route('integration.tinymce') }}">TinyMce</a></li>
+                <li><a href="{{ route('integration.standard-alone-button') }}">Standard alone button</a></li>
+                <li><a href="https://goo.gl/BTYHda" target="_blank">Purchase</a></li>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Language <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a href="?locale=en">English</a></li>
+                        <li><a href="?locale=vi">Vietnamese</a></li>
+                    </ul>
+                </div>
+            </ul>
+        </div>
     </div>
-
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('vendor/media/packages/bootstrap-summernote/summernote.css') }}" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('vendor/media/packages/font-awesome/css/font-awesome.min.css') }}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="{{ asset('vendor/media/packages/bootstrap-summernote/summernote.min.js') }}"></script>
-    @include('media::partials.media')
-    <script src="{{ asset('vendor/media/js/jquery.addMedia.js') }}"></script>
-    <script>
-        'use strict';
-
-        $(document).ready(function () {
-            $('.summernote').summernote({
-                toolbar: [
-                    ['style', ['style']],
-                    ['font', ['bold', 'italic', 'underline', 'clear']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['insert', ['link']],
-                    ['misc', ['fullscreen']]
-                ],
-                height: 300
-            });
-        });
-    </script>
-@endsection
+</nav>
+@yield('content')
+</body>
+</html>
