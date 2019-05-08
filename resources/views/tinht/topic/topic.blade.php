30ffb7201305a0d7854c78fba980176077b0343e
dangquocdung <dungthinhvn@gmail.com>
2019-05-08 13:16:16 +0700

    aádsa

--- resources/views/tinht/topic/topic.blade.php
+++ resources/views/tinht/topic/topic.blade.php
@@ -206,7 +206,7 @@
                     <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
                         <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="/tinht/img/spin.svg" />
                     </div>
-                    <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:980px;height:600px;overflow:hidden;">
+                    <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:100%;height:600px;overflow:hidden; background-color:black">
                         @foreach($Topic->photos as $photo)
                             <div data-p="170.00">
                                 <img data-u="image" src="{{ URL::to('uploads/topics/'.$photo->file) }}" alt="{{ $photo->title  }}">
