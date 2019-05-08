75407bbd7607a42fdf7f2c00e595d724de1c9593
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro-3.local>
2019-05-08 09:52:39 +0700

    adasd

--- resources/views/tinht/home/video.blade.php
+++ resources/views/tinht/home/video.blade.php
@@ -1,7 +1,28 @@
-<div class="col-md-12">
+<div class="col-md-6">
     <div class="Head pos-rel clearfix">
         <h2 class="ParentCate left">
-            <a href="#">Video clips</a>
+            <a href="#">Du lịch Hà Tĩnh</a>
+        </h2>
+        <span class="line-red">.</span>
+    </div>
+
+    <div class="RYPP r16-9" data-playlist="PLh_HV-iv7X8PHVkqrcy9qegZKJu-nMbbL">
+        <div class="RYPP-video">
+            <div class="RYPP-video-player">
+            <!-- Will be replaced -->
+            </div>
+        </div>
+        <div class="RYPP-playlist">
+            <div class="RYPP-items customScroll"></div>
+        </div>
+    </div>
+            
+</div>
+
+<div class="col-md-6">
+    <div class="Head pos-rel clearfix">
+        <h2 class="ParentCate left">
+            <a href="#">Thể thao</a>
         </h2>
         <span class="line-red">.</span>
     </div>
