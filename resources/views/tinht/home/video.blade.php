60e69ec54c727ccd9cb0be57f6bfbefcd1e4ac2f
dangquocdung <dungthinhvn@gmail.com>
2019-05-09 12:10:57 +0700

    asd

--- resources/views/tinht/home/video.blade.php
+++ resources/views/tinht/home/video.blade.php
@@ -8,7 +8,7 @@
     
         <div class="clearfix"></div>
     
-        <div class="four-column-post-carousel column-post-carousel post-block-carousel dark life-style-post-carousel row">
+        <div class="three-column-post-carousel column-post-carousel post-block-carousel dark life-style-post-carousel row">
     
             @foreach ($Photo as $Topic )
                 @php
